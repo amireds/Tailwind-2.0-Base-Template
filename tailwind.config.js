ffef7b16716fb925177b6e6e6c6ba56346f8e076
amireds <zoe_steve@hotmail.com>
2021-01-18 00:19:11 +0100

    Tailwind 2.0 Base template

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors');
+module.exports = {
+  purge: {
+    mode: 'layers',
+    content: ['./public/**/*.html', './public/**/*.js'],
+  },
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        lightblue: colors.lightBlue,
+        gray:colors.trueGray,
+      },
+    },
+  },
+  variants: {
+    extend: {
+      ringColor: ['hover'],
+      ringOpacity: ['hover'],
+      ringWidth: ['hover'],
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
